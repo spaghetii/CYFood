dcfba6a2edd8f003be67aa0703059f3d230be96a
spaghetii <joe.sun.m.j@gmail.com>
2019-09-24 15:33:14 +0800

    clear

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,8 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-{{-- test branch --}}
-{{-- test2 --}}
+                
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
