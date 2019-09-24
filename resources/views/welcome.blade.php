777091d83e43a7e938b3111448199abb45dbaa89
rinfeng3370 <rinfeng3371@gmail.com>
2019-09-24 22:17:34 +0800

    Merge test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,6 +82,7 @@
             <div class="content">
                 <div class="title m-b-md">
                     Laravel
+                    林培誠 練習merge
                 </div>
                 {{-- test --}}
                 {{-- test2 --}}
