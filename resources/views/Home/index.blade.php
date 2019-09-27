97ac38109fdaf9d662f4b1975ba1d708d9f17e06
AllenHuang <sa810713@gmail.com>
2019-09-27 17:42:23 +0800

    20190927

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -1,60 +1,155 @@
 @extends('layouts.layout')
+
+{{-- 隱藏式 header 地址搜尋 --}}
+@section('headerSearchLarge')
+    <div class="input-group navbar col-sm-6" id="headerSearchLarge">
+        <input type="text" class="form-control" placeholder="請輸入地址" value="台中市南屯區公益路二段51號">
+        <div class="input-group-append">
+            <button class="btn btn-warning" type="submit" id="SearchAddressButton">Go</button>
+        </div>
+    </div>
+@endsection
+
 @section('content')
 
-            <div class="mainHead">
-                <div class="mainSearchDiv">
-                    <div>
-                        <h1 class="mb-5">把您想吃的美食外送到府</h1>
-                    </div>
-                    <div class="container">
-                        <!-- 區域選擇 -->
-                        <div class="btn-group col-sm-2 col-12" id="selectArea">
-                            <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                選擇區域
-                            </button>
-                            <div class="dropdown-menu">
-                                <a class="dropdown-item" href="">西區</a>
-                                <a class="dropdown-item" href="">北區</a>
-                                <a class="dropdown-item" href="">東區</a>
-                                <a class="dropdown-item" href="">南區</a>
-                                <a class="dropdown-item" href="">中區</a>
-                                <a class="dropdown-item" href="">西屯區</a>
-                                <a class="dropdown-item" href="">南屯區</a>
-                                <a class="dropdown-item" href="">北屯區</a>
-                            </div>
-                        </div>
-                        <!-- 類別篩選 -->
-                        <div class="btn-group col-sm-2 col-12" id="selectCategory">
-                                <button type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"
-                                    aria-haspopup="true" aria-expanded="false">
-                                    選擇類別
-                                </button>
-                                <div class="dropdown-menu">
-                                    <a class="dropdown-item" href="">中式料理</a>
-                                    <a class="dropdown-item" href="">日式料理</a>
-                                    <a class="dropdown-item" href="">美式料理</a>
-                                    <a class="dropdown-item" href="">韓式料理</a>
-                                    <a class="dropdown-item" href="">泰式料理</a>
-                                    <a class="dropdown-item" href="">義式料理</a>
-                                    <a class="dropdown-item" href="">甜點</a>
-                                    <a class="dropdown-item" href="">飲料</a>
-                                </div>
-                            </div>
-                        <!-- 地址搜尋 -->
-                        <div class="input-group col-sm-8 col-12" id="SearchAddress">
-                            <input type="text" class="form-control" placeholder="請輸入地址" value="台中市南屯區公益路二段51號">
-                            <div class="input-group-append">
-                                <button class="btn btn-warning" type="submit" id="SearchAddressButton">Go</button>
-                            </div>
-                        </div>
+    <div class="mainHead">
+        <div class="mainSearchDiv">
+            <div>
+                <h1 class="mb-4 col-sm-12">把您想吃的美食外送到府</h1>
+            </div>
+            <div class="container">  
+                <!-- 地址搜尋 -->
+                <div class="input-group col-sm-12 col-12" id="SearchAddress">
+                    <input type="text" class="form-control" placeholder="請輸入地址" value="台中市南屯區公益路二段51號">
+                    <div class="input-group-append">
+                        <button class="btn btn-warning" type="submit" id="SearchAddressButton">Go</button>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>  
+    <!-- 本日推薦 -->
+    <div class="container-fluid" id="todayRecommend">
+        <div>
+            <h3>本日推薦</h3>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 mt-5">
+                <div class="todayRecommendImage"></div>
+                <div class="todayRecommendContent">
+                    <div class="row mt-2">
+                        可不可熟成紅茶 中佑店
+                    </div>
+                    <div class="row mt-2">
+                        <div class="mr-1 todayRecommendContentItem"><small>$.飲料</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><img src="img/star1.png" class="mr-1"><small>4.7/5</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><small>60TWD 費用</small></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-5">
+                <div class="todayRecommendImage"></div>
+                <div class="todayRecommendContent">
+                    <div class="row mt-2">
+                        可不可熟成紅茶 中佑店
+                    </div>
+                    <div class="row mt-2">
+                        <div class="mr-1 todayRecommendContentItem"><small>$.飲料</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><img src="img/star1.png" class="mr-1"><small>4.7/5</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><small>60TWD 費用</small></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-5">
+                <div class="todayRecommendImage"></div>
+                <div class="todayRecommendContent">
+                    <div class="row mt-2">
+                        可不可熟成紅茶 中佑店
+                    </div>
+                    <div class="row mt-2">
+                        <div class="mr-1 todayRecommendContentItem"><small>$.飲料</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><img src="img/star1.png" class="mr-1"><small>4.7/5</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><small>60TWD 費用</small></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-5">
+                <div class="todayRecommendImage"></div>
+                <div class="todayRecommendContent">
+                    <div class="row mt-2">
+                        可不可熟成紅茶 中佑店
+                    </div>
+                    <div class="row mt-2">
+                        <div class="mr-1 todayRecommendContentItem"><small>$.飲料</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><img src="img/star1.png" class="mr-1"><small>4.7/5</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><small>60TWD 費用</small></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-5">
+                <div class="todayRecommendImage"></div>
+                <div class="todayRecommendContent">
+                    <div class="row mt-2">
+                        可不可熟成紅茶 中佑店
+                    </div>
+                    <div class="row mt-2">
+                        <div class="mr-1 todayRecommendContentItem"><small>$.飲料</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><img src="img/star1.png" class="mr-1"><small>4.7/5</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><small>60TWD 費用</small></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4 mt-5">
+                <div class="todayRecommendImage"></div>
+                <div class="todayRecommendContent">
+                    <div class="row mt-2">
+                        可不可熟成紅茶 中佑店
+                    </div>
+                    <div class="row mt-2">
+                        <div class="mr-1 todayRecommendContentItem"><small>$.飲料</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><img src="img/star1.png" class="mr-1"><small>4.7/5</small></div>
+                        <div class="mr-1 todayRecommendContentItem"><small>60TWD 費用</small></div>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    </div>
+    
+    {{-- 餐廳加入 div --}}
+    <div class="restaurantJoinUs">
+        <div class="restaurantJoinUsContent">
+            <div>
+                <p><h3>加入CY Food，把你的美食分享給大家！</h3></p>
+                <p>想要上千名新顧客試試你的美食嗎？我們想，相信你也是！</p>
+                <p>達到這個目的超簡單：</p>
+                <p>我們把你的菜單放上線、幫你處理訂單、到餐廳去取餐、將餐點外送給顧客們！</p>
+                <p>還等什麼？一起和我們開啟這個外送美食給大家的旅程吧！</p>
+            </div>
+            <div>
+                <a href="" class="btn btn-warning" id="restaurantJoinBtn" role="button" aria-pressed="true">立即加入</a>
+            </div>
+        </div>
+    </div>
+    
+    {{-- 與footer分隔線 --}}
+    <div class="container-fluid separationline">
+        <hr>
+    </div>   
 
-            <script>
-                    $("#SearchAddressButton").on("click",function(){
-                        alert('oops!');
-                    });
-            </script>
+    <script>
+        $("#SearchAddressButton").on("click",function(){
+            alert('oops!');
+        });  
+        
+        // 隱藏式 header 地址搜尋 顯示判斷
+        $(window).scroll(function () {
+                var winTop = $("#headerSearchLarge").offset().top;
+                var navTop = $("#SearchAddress").offset().top;
+                if (winTop > navTop) {
+                    $("#headerSearchLarge").css("visibility","visible");
+                }else{
+                    $("#headerSearchLarge").css("visibility","hidden");
+                }
+        })
+    </script>
 @endsection
