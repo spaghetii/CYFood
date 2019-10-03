977cfab5ad608ebe79a30514a399a7b9487acfda
Jennifer <filtoho@gmail.com>
2019-10-03 17:04:06 +0800

    jen1003

---
+++ resources/views/layouts/clientLayout.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <!-- vue -->
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+    {{-- Topcss --}}
+    <link rel="stylesheet" href="/css/clientTop.css">
+    
+</head>
+<body>
+    <div class="container-fuild">
+        <!-- top -->
+        <div class="row no-gutters sticky-top" id="fixedDiv">
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <a href="/newOrder">
+                    <div class="fixedItem rounded">
+                        <img src="/img/client/neworder.png" alt="">
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <a href="/processing">
+                    <div class="fixedItem rounded">
+                        <img src="/img/client/processing.png" alt="">
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <a href="/takeout">
+                    <div class="fixedItem rounded">
+                        <img src="/img/client/takeout.png" alt="">
+                    </div>
+                </a>
+            </div>
+            <div class="col-lg-3 col-md-6 col-sm-12">
+                <a href="/user">
+                    <div class="fixedItem rounded">
+                        <img src="/img/client/user.png" alt="">
+                    </div>
+                </a>
+            </div>
+        </div>
+
+        {{-- bottom --}}
+        @yield('content')
+
+    </div>
+    
+    @yield('script')
+
+</body>
+</html>
