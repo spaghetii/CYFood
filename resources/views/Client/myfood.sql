8483fde778e509b2cec756f1ab420d80f6fe5a5d
Jennifer <filtoho@gmail.com>
2019-10-04 17:08:23 +0800

    05:08

--- resources/views/Client/myfood.sql
+++ resources/views/Client/myfood.sql
@@ -4,3 +4,16 @@ values ('CY20191004001',
 '[{"memberName":"Jennifer","mealQuantity":"1","mealName":"冰淇淋紅茶","mealUnitPrice":"70"},
     {"memberName":"Jennifer","mealQuantity":"2","mealName":"珍珠奶茶","mealUnitPrice":"60"}]',
 '2019/10/04','2019/10/04','0',1,34);
+insert into orders 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,ordersFinish,memberId,mealId) 
+values ('CY20191004002',
+'[{"memberName":"Panda","mealQuantity":"4","mealName":"紅茶","mealUnitPrice":"10"},
+    {"memberName":"Panda","mealQuantity":"5","mealName":"奶茶","mealUnitPrice":"20"}]',
+'2019/10/05','2019/10/05','0',2,33);
+insert into orders 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,ordersFinish,memberId,mealId) 
+values ('CY20191004003',
+'[{"memberName":"Bear","mealQuantity":"2","mealName":"可不可紅茶超級大杯","mealUnitPrice":"66"},
+    {"memberName":"Bear","mealQuantity":"50","mealName":"超級滷肉飯","mealUnitPrice":"100"},
+    {"memberName":"Bear","mealQuantity":"1","mealName":"青菜","mealUnitPrice":"30"}]',
+'2019/10/05','2019/10/05','0',2,33);
