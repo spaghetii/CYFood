91f7bebb0166a32e26c6b42253998a4884bf1d3f
rinfeng3370 <rinfeng3371@gmail.com>
2019-10-04 13:43:13 +0800

    忘記密碼完成 404page 註冊驗證

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+
+@section('content')
+
+<div class="container">
+    <p class="display-4 text-danger text-center mt-5">Oops! The page you are looking for can't be found.</p>
+</div>
+
+
+@endsection
