32ebc0a6ce97313a476732e815aa72747f3b329b
spaghetii <joe.sun.m.j@gmail.com>
2019-10-08 16:45:47 +0800

    test

--- resources/views/Client/myfood.sql
+++ resources/views/Client/myfood.sql
@@ -1,21 +1,21 @@
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004001',
 '{"restaurant":"CY","memberName":"Jennifer","meal":[{"mealQuantity":"1","mealName":"冰淇淋紅茶","mealUnitPrice":"70",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"2","mealName":"珍珠奶茶","mealUnitPrice":"70",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/04','2019/10/04','1');
+'2019/10/04','2019/10/04','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004002',
 '{"restaurant":"CY5","memberName":"Panda","meal":[{"mealQuantity":"4","mealName":"紅茶","mealUnitPrice":"10",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"2","mealName":"奶茶","mealUnitPrice":"70",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004003',
 '{"restaurant":"CY","memberName":"Bear","meal":[{"mealQuantity":"2","mealName":"可不可紅茶超級大杯","mealUnitPrice":"66",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
@@ -23,9 +23,9 @@ values ('CY20191004003',
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
     {"mealQuantity":"1","mealName":"青菜","mealUnitPrice":"30",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004004',
 '{"restaurant":"CY5","memberName":"Lebron","meal":[{"mealQuantity":"2","mealName":"可不可紅茶超級大杯","mealUnitPrice":"66",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
@@ -33,9 +33,9 @@ values ('CY20191004004',
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
     {"mealQuantity":"1","mealName":"青菜不要高麗菜","mealUnitPrice":"199",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004005',
 '{"restaurant":"CY","memberName":"Chris","meal":[{"mealQuantity":"2","mealName":"可不可紅茶超級大杯中杯小杯","mealUnitPrice":"66",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
@@ -43,9 +43,9 @@ values ('CY20191004005',
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
     {"mealQuantity":"1","mealName":"青菜不要高麗菜","mealUnitPrice":"199",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004006',
 '{"restaurant":"CY3","memberName":"Chris","meal":[{"mealQuantity":"2","mealName":"可不可紅茶超級大杯中杯小杯","mealUnitPrice":"66",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
@@ -55,9 +55,9 @@ values ('CY20191004006',
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"1","mealName":"青菜不要高麗菜","mealUnitPrice":"199",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004007',
 '{"restaurant":"CY3","memberName":"克里斯伊凡","meal":[{"mealQuantity":"2","mealName":"可不可紅茶超級大杯中杯小杯","mealUnitPrice":"66",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
@@ -67,9 +67,9 @@ values ('CY20191004007',
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"1","mealName":"青菜不要高麗菜","mealUnitPrice":"199",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004009',
 '{"restaurant":"CY","memberName":"克里斯伊凡","meal":[{"mealQuantity":"2","mealName":"可不可紅茶超級大杯中杯小杯","mealUnitPrice":"66",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
@@ -79,23 +79,23 @@ values ('CY20191004009',
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"1","mealName":"青菜不要高麗菜","mealUnitPrice":"199",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004010',
 '{"restaurant":"CY2","memberName":"霸王花","meal":[{"mealQuantity":"1","mealName":"白開水","mealUnitPrice":"10",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"1","mealName":"衛生紙","mealUnitPrice":"20",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 insert into orders 
-(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus) 
+(ordersNum,ordersDetails,ordersCreate,ordersUpdate,OrdersStatus,memberId,shopId) 
 values ('CY20191004011',
 '{"restaurant":"CY2","memberName":"蚊香蝌蚪","meal":[{"mealQuantity":"1","mealName":"蚊香","mealUnitPrice":"10",
     "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] },
    {"mealQuantity":"1","mealName":"青蛙","mealUnitPrice":"20",
    "mealDetail":[{"type":0,"mealNum":"meal0","detail": "","price":"","check":false}] } ]}',
-'2019/10/05','2019/10/05','1');
+'2019/10/05','2019/10/05','1',1,1);
 
 
 
